2bde94c21ba8bc3f57bf7441d95b43d0f22aa7e3
technoY2K <cloud@kevia.me>
2022-02-19 18:46:35 -0800

    feat: traffic light

--- app/Main.hs
+++ app/Main.hs
@@ -2,8 +2,15 @@ module Main where
 
 import           CodeWorld
 
-circles :: Picture
-circles = colored green (solidCircle 1) & solidCircle 2
+greenLight :: Picture
+greenLight = colored green (translated 0 (-1.5) (solidCircle 1))
+
+redLight :: Picture
+redLight = colored red (translated 0 (1.5) (solidCircle 1))
+
+frame = rectangle 2.5 5.5
+
+trafficLight = greenLight & redLight & frame
 
 main :: IO ()
-main = drawingOf circles
+main = drawingOf trafficLight
