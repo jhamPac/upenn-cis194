05bbc1a4531c593679723c70fd4690596dd83f35
technoY2K <cloud@kevia.me>
2022-03-01 10:10:32 -0700

    chore: complete Maze module

--- src/Maze.hs
+++ src/Maze.hs
@@ -1,4 +1,4 @@
-module Maze where
+module Maze (pictureOfMaze) where
 
 import           CodeWorld
 
@@ -9,4 +9,38 @@ ground = colored yellow (solidRectangle 1 1)
 storage = solidCircle 0.3 & ground
 box = colored brown (solidRectange 1 1)
 
+data Tile
+    = Wall
+    | Ground
+    | Storage
+    | Box
+    | Blank
 
+drawTile :: Tile -> Picture
+drawTile Wall    = wall
+drawTile Ground  = ground
+drawTile Storage = storage
+drawTile Box     = box
+drawTile Blank   = blank
+
+pictureOfMaze :: Picture
+pictureOfMaze = draw21Times (\r -> draw21Times (\c -> drawTileAt r c))
+
+draw21Times :: (Integer -> Picture) -> Picture
+draw21Times f = go (-10)
+    where
+        go :: Integer -> Picture
+        go 11 = blank
+        go n  = f n & go (n + 1)
+
+drawTileAt :: Integer -> Integer -> Picture
+drawTileAt r c = translated (fromIntegral r) (fromIntegral c) (drawTile (maze r c))
+
+maze :: Integer -> Integer -> Tile
+maze x y
+    | abs x > 4 || abs y > 4 = Blank
+    | abs x == 4 || abs y == 4 = Wall
+    | x == 2 && y <= 0 = Wall
+    | x == 3 && y <= 0 = Storage
+    | x >= -2 && y == 0 = Box
+    | otherwise = Ground
