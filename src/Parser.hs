9d5c03e2e8941c104b7bf8b366c9ec3ba455e01a
technoY2K <cloud@kevia.me>
2022-03-07 16:57:36 -0700

    feat: helper functions for Parser

--- src/Parser.hs
+++ src/Parser.hs
@@ -1,3 +1,24 @@
 module Parser where
 
+import           Data.Char
+import           Data.List
+import           Data.Maybe
+import           System.Environemnt
+import           System.Exit
+import           System.IO
+
 newtype Parser a = P (String -> Maybe (a, String))
+
+runParser :: Parser a -> String -> Maybe (a, String)
+runParser (P p) = p
+
+parse :: Parser a -> String -> Maybe a
+parse p input = case runParser p input of
+    Just (result, "") -> Just result
+    _                 -> Nothing
+
+noParserP :: Parser a
+noParserP = P (\_ -> Nothing)
+
+pureParserP :: a -> Parser a
+pureParserP x = P (\input -> Just (x, input))
