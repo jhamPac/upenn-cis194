dba644a4b2c5044bfdb767cfa51c61759627ee51
technoY2K <cloud@kevia.me>
2022-03-07 17:07:00 -0700

    feat: make Parser an instance of Functor, Applicative and Monad

--- src/Parser.hs
+++ src/Parser.hs
@@ -22,3 +22,23 @@ noParserP = P (\_ -> Nothing)
 
 pureParserP :: a -> Parser a
 pureParserP x = P (\input -> Just (x, input))
+
+instance Functor Parser where
+    fmap f p = P p'
+        where
+            p' input = case runParser p input of
+                Just (result, rest) -> Just (f result, rest)
+                Nothing             -> Nothing
+
+instance Applicative Parser where
+    pure = pureParserP
+    p1 <*> p2 = P $ \input -> do
+        (f, r) <- runParser p1 input
+        (x, r') <- runParser p2 r
+        return (f x, r')
+
+instance Monad Parser where
+    return = pure
+    p1 >>= k = P $ \input -> do
+        (x, r1) <- runParser p1 input
+        runParser (k x) r1
